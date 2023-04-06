5d61633a8a436e5e8117a666f0272b0c0824ca49
Tom Carroll <tcarroll@fnal.gov>
2023-04-06 12:56:42 -0500

    setting up resfixfix options to test for genie skew

--- Setup.bash
+++ Setup.bash
@@ -1,2 +1,9 @@
 
 source /cvmfs/nova.opensciencegrid.org/novasoft/slf6/novasoft/setup/setup_nova.sh -r S22-09-15
+
+unsetup genie_xsec
+unsetup genie_phyopt
+
+setup genie_xsec v3_00_06 -qN1810j0211a:e1000:k250:resfixfix
+
+setup genie_phyopt v3_00_06 -qdkcharmtau:resfixfix
--- jobScript.sh
+++ jobScript.sh
@@ -7,6 +7,13 @@ HIST_NAME=${3}
 
 source /cvmfs/nova.opensciencegrid.org/novasoft/slf6/novasoft/setup/setup_nova.sh -s -r S22-09-15
 
+unsetup genie_xsec
+unsetup genie_phyopt
+
+setup genie_xsec v3_00_06 -qN1810j0211a:e1000:k250:resfixfix
+
+setup genie_phyopt v3_00_06 -qdkcharmtau:resfixfix
+
 if [ "$OUTDIR" = "-t" ];
 then
     gevgen -r 777 -n 20000 -p 14 -t 1000260560 -e 0,40 -f ${FLUX_FILE},${HIST_NAME} --seed 777 --cross-sections $GENIEXSECFILE --tune $GENIE_XSEC_TUNE
