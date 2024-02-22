25fe8898375f8cf7a48e7f4e42319ba8ed6b5917
Tom Carroll <tcarroll@fnal.gov>
2024-02-22 11:56:38 -0600

    Corrected typo in the output describing the job inputs.

--- SubmitJobs.sh
+++ SubmitJobs.sh
@@ -4,7 +4,10 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
-
+Number_of_Jobs=${1}
+PDG_PARTICLE=${2}
+FLUX_FILE=${3}
+HIST_NAME=${4}
 
 OUTDIR=/pnfs/minos/scratch/users/tcarroll/MINOSgenie_$(date +"%Y_%m_%d_%I_%M_%p")
 
@@ -15,17 +18,15 @@ OUTDIR=${OUTDIR}_${COUNT}
 
 mkdir -p ${OUTDIR}
 
-PDG_PARTICLE=${2}
-FLUX_FILE=${3}
-HIST_NAME=${4}
+
 
 cat <<EOF >${OUTDIR}/inputs.txt
 Number of jobs: ${1}
-Particle PDF ID: ${2}
+Particle PDG ID: ${2}
 Flux file: ${3}
 Histogram name: ${4}
 EOF
 
 FLUX_FILE_ARG=`basename ${FLUX_FILE}`
 
-jobsub_submit -G minos -Q -N ${1} --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --memory=2GB --expected-lifetime=60m --site=FermiGrid,FNAL,Caltech,BNL,Michigan,BU,Clemson,Colorado,Cornell,Hyak_CE,MIT,Nebraska,NotreDame,Stampede,HOSTED_STANFORD,SU-ITS,TTU,UCSD,Wisconsin --lines '+FERMIHTC_AutoRelease=True' --lines '+FERMIHTC_GraceLifetime=1800' -f dropbox://${FLUX_FILE} --use-pnfs-dropbox file://jobScript.sh ${OUTDIR} ${PDG_PARTICLE} ${FLUX_FILE_ARG} ${HIST_NAME}
+jobsub_submit -G minos -Q -N ${Number_of_Jobs} --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --memory=2GB --expected-lifetime=60m --site=FermiGrid,FNAL,Caltech,BNL,Michigan,BU,Clemson,Colorado,Cornell,Hyak_CE,MIT,Nebraska,NotreDame,Stampede,HOSTED_STANFORD,SU-ITS,TTU,UCSD,Wisconsin --lines '+FERMIHTC_AutoRelease=True' --lines '+FERMIHTC_GraceLifetime=1800' -f dropbox://${FLUX_FILE} --use-pnfs-dropbox file://jobScript.sh ${OUTDIR} ${PDG_PARTICLE} ${FLUX_FILE_ARG} ${HIST_NAME}
